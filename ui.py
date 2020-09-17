f9590c687ff782319459d6c199b15dc86a616c3e
micha <jw0711qt@go.minnstate.edu>
2020-09-17 11:32:51 -0500

    add blank line before and after a list of books

--- ui.py
+++ ui.py
@@ -26,12 +26,14 @@ def show_books(books):
      :param books: the book list """
 
     # TODO print a blank line before and after the book list or no books message
-
+    
     if books:
         for book in books:
             print(book)
     else:
         print('No books to display')
+
+    
 
 
 def get_book_info():
