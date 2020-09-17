3b5f3c330329851bbf24ebf58de9060efd7cb054
micha <jw0711qt@go.minnstate.edu>
2020-09-17 13:28:52 -0500

    added space

--- ui.py
+++ ui.py
@@ -27,15 +27,14 @@ def show_books(books):
 
     # TODO print a blank line before and after the book list or no books message
     
-    print
-
+    
+    print()
     if books:
         for book in books:
             print(book)
     else:
         print('No books to display')
-
-    print()
+    prin()
 
 
 
