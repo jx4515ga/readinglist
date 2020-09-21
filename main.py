942f99aaa06938d63e144f9bc4b683a32ed97c14
Paul <boris.pizha@go.minneapolis.edu>
2020-09-20 20:59:05 -0500

    added delete to menu

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def main():
 
 def create_menu():
     menu = Menu()
-    menu.add_option('9', 'Delete Book', delete_book)
     menu.add_option('1', 'Add Book', add_book)
     menu.add_option('2', 'Search For Book', search_book)
     menu.add_option('3', 'Show Unread Books', show_unread_books)
     menu.add_option('4', 'Show Read Books', show_read_books)
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('7', 'Delete Book', delete_book)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -86,7 +86,8 @@ def delete_book():
         print("Book was deleted") 
 
     except:
-        print("Error. Book not found. Try again")   
+
+        print("Book not found. Try again")   
 
 def quit_program():
     ui.message('Thanks and bye!')
